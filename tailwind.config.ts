23bd13e999ef0b25525b28ece92630ea952d8c32
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-05-27 23:13:21 -0300

    start

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.tsx",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
