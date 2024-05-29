91351d18656b70a536e81b7e651d1c191152b7ae
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-05-28 23:15:14 -0300

    att

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,21 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/**@type {import('tailwindcss').Config}*/
+module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      colors: {},
+      gridTemplateColumns: {
+        app: 'minxmax(18rem, 20rem) 1fr',
+      },
+
+    maxWidth: {
+      app:'700px',
+    },
+   
+      colors: {
+        test: '#333',
+      },
     },
   },
   plugins: [],
 }
-export default config
+
