e4bc67862cd94f558668c45c3d3e81ded91db3d0
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-05-29 22:01:49 -0300

    att

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -8,7 +8,7 @@ export function Sidebar() {
     
         <div className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2">
             <Search className="h-5 w-5 text-zinc-500" /> 
-            
+            <input className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" placeholder="Search" />
         </div>
     </aside>
     )
