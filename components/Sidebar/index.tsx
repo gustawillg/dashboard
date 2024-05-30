c92469e9598b53c3310c97873db41801b022b3c3
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-05-30 17:21:40 -0300

    👻

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -3,19 +3,19 @@ import { BarChart, CheckSquare, Cog, Flag, Home, Search, SquareStack, Users } fr
 import { NavItem } from "./Navitem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
+import { InputControl, InputPrefix, InputRoot } from "../input";
 
 export function Sidebar() {
  return (
  <aside className='flex flex-col gap-6 border-r border-zinc-200 px-5 py-8'>
     <Logo/>
     
-        <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm mt-6">
-            <Search className="h-5 w-5 text-zinc-500" /> 
-            <input 
-            className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600" 
-            placeholder="Search" 
-            />
-        </div>
+        <InputRoot>
+            <InputPrefix>
+                <Search className="h-5 w-5 text-zinc-500" />
+            </InputPrefix>
+            <InputControl />
+        </InputRoot>
 
         <nav className="space-y-0.5 ">
             <NavItem title="Home" icon={Home} />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 
 import { ReactNode } from 'react'
 import { Inter } from 'next/font/google'
-import { Sidebar } from '../../components/Sidebar'
+import { Sidebar } from '@/components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="relative min-h-screen lg:grid lg:grid-cols-app">
+        <div className="relative min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
           <Sidebar />
 
           <main className="max-w-screen px-4 pb-12 pt-24 lg:col-start-2 lg:w-auto lg:px-8 lg:pt-8">
